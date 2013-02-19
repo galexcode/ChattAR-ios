c4d6d6c24733a2350693f166065a2f006e4a87d9
Kirill Ushkov <kirillushkov2010@gmail.com>
2013-02-19 11:14:18 +0200

    added parent view controller class

---
+++ Chattar/Classes/Controllers/CommonViewController/CommonViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommonViewController.h
+//  Chattar
+//
+//  Created by kirill on 2/19/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonViewController : UIViewController
+
+@end
---
+++ Chattar/Classes/Controllers/CommonViewController/CommonViewController.m
@@ -0,0 +1,38 @@
+//
+//  CommonViewController.m
+//  Chattar
+//
+//  Created by kirill on 2/19/13.
+//
+//
+
+#import "CommonViewController.h"
+
+@interface CommonViewController ()
+
+@end
+
+@implementation CommonViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
